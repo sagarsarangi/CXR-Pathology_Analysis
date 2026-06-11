b469923410ffd78d31e005b378e002ec786160f5
sagarsarangi <sarangisagar9@gmail.com>
2026-06-11 13:13:11 +0530

    auth layout changes

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col bg-primary p-6">
+    <div className="min-h-screen flex flex-col bg-primary p-6 ">
       {/* Spacer to push content below the fixed navbar */}
-      <div className="h-24 w-full shrink-0" />
+      {/* <div className="h- w-full shrink-0" /> */}
       
-      <div className="flex-1 flex items-center justify-center">
+      <div className="flex-1 flex items-center justify-center mt-14 pb-4">
         <div className="w-full max-w-md bg-surface/50 backdrop-blur-xl p-8 rounded-container border border-accent/20">
           {children}
         </div>
